fe9bdac9f264d067e00d1bfe11b8f45b3efef010
evgen <parom.evgen@mail.ru>
2024-05-04 19:58:26 +0300

    Producer with Keys

---
+++ kafka-basic/src/main/java/io/conduktor/demo/kafka/ProducerDemoKeys.java
@@ -0,0 +1,61 @@
+package io.conduktor.demo.kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoKeys {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer!");
+
+        // create Producer properties
+        Properties properties = new Properties();
+        // connect to Localhost
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 10; i++) {
+
+                String topic = "demo_java";
+                String key = "id_" + i;
+                String value = "message " + i;
+
+                // create a Producer Record
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, key, value);
+
+                // send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        // executes every time a record successfully sent or an exception is thrown
+                        if (e == null) {
+                            // the record was successfully sent
+                            log.info("Key: " + key + " | Partition: " + metadata.partition());
+                        } else {
+                            log.error("Error while producing", e);
+                        }
+                    }
+                });
+            }
+        }
+
+        // flush and close the Producer
+        producer.flush();// tell the producer to send all data and block until done --synchronous
+        producer.close();
+    }
+}
